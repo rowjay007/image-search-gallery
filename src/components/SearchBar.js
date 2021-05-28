e03bff7b22989e5952d35cd5a4b873555c346a07
rowjay007 <rowlives@gmail.com>
2021-05-28 12:36:56 +0100

    creating form with react-hook-form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,16 +3,25 @@ import React from "react";
 import { useForm } from "react-hook-form";
 
 const SearchBar = () => {
-  const { register, handleSubmit } = useForm();
-  //   const onSubmit = (data) => alert(JSON.stringify(data));
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div className="ui segment">
-      <form className="ui form" onSubmit={handleSubmit}>
+      <form className="ui form" onSubmit={handleSubmit(onSubmit)}>
         <div className="field">
           <label> Image Search</label>
-          {/* <input {...register("search image")} placeholder="search image" /> */}
- 
-          <input type="text" />
+          <input
+            {...register("searchImage", { required: true })}
+            type="text"
+            placeholder="search image"
+          />
+
+          {errors.searchImage && <span>This field is required</span>}
         </div>
       </form>
     </div>
