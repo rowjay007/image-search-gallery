6e6a0ff57851b72dc81abfc7846871d634f22054
rowjay007 <rowlives@gmail.com>
2021-05-31 10:42:12 +0100

    installing axios to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import SearchBar from "./SearchBar";
 import "semantic-ui-css/semantic.min.css";
 
+// access key = "nweoVxPQdGa-w6qfhXvzt2CQh5SH71OzWpReN77Zb7I"
+
 
 function App() {
   const onSearchSubmit = (e) => {
