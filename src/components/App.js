fb711f16a6648b99552d421f9dbc979ba5f2db31
rowjay007 <rowlives@gmail.com>
2021-05-31 12:06:29 +0100

    create environment varables

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import "semantic-ui-css/semantic.min.css";
+// require('dotenv').config()
 
-// access key = "nweoVxPQdGa-w6qfhXvzt2CQh5SH71OzWpReN77Zb7I"
-
+// access key = ""
 
 function App() {
   const onSearchSubmit = (e) => {
-    console.log(e)
-  }
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: e },
+      headers: {
+        Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
+      },
+    });
+    console.log(e);
+  };
   return (
     <div className="ui container" style={{ marginTop: "10px" }}>
       <SearchBar onSearchSubmit={onSearchSubmit} />
