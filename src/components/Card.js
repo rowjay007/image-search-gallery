51a8912c08abf6f000245b8790c19f485fefac18
rowjay007 <rowlives@gmail.com>
2021-06-04 12:47:14 +0100

    converting Card componenets to functional components and destructing items

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,23 @@
-// import React from "react";
+import React from "react";
 
-// const Card = ({ image, urls, description }) => {
-//   return (
-//     <div>
-//       <img src={image.urls} alt={image.description} />
-//     </div>
-//   );
-// };
+const Card = ({ image, urls, description }) => {
+  return (
+    <div>
+      <img src={image.urls.regular} alt={image.description} />
+    </div>
+  );
+};
 
-// export default Card;
+export default Card;
 
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export default class Card extends Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image.urls.regular} alt={this.props.image.description} />
-      </div>
-    );
-  }
-}
+// export default class Card extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <img src={this.props.image.urls.regular} alt={this.props.image.description} />
+//       </div>
+//     );
+//   }
+// }
