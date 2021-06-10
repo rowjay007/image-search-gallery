cf28ba2416531872dfa60911030cdb14a73134a3
rowjay007 <rowlives@gmail.com>
2021-06-10 11:09:14 +0100

    modifying readme file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState, useRef } from "react";
 import { CardList } from "./CardStyled";
 
-
 const Card = ({ image, urls, description }) => {
-
   const imageRef = useRef();
   const [span, setSpans] = useState(0);
 
-  
   useEffect(() => {
     const ImageHandle = () => {
       const height = imageRef.current.clientHeight;
@@ -17,13 +14,14 @@ const Card = ({ image, urls, description }) => {
 
     imageRef.current.addEventListener("load", ImageHandle);
   }, []);
-  
+
   return (
     <div style={{ gridRow: `span ${span}` }}>
       <CardList
         ref={imageRef}
         src={image.urls.regular}
         alt={image.description}
+        
       />
     </div>
   );
