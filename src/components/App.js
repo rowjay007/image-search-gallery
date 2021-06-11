e57b82dca81f24d370e1d7b6def0f31e886dbbb0
rowjay007 <rowlives@gmail.com>
2021-06-11 10:44:48 +0100

    restructured api

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,22 @@
 import { useContext } from "react";
 import SearchBar from "./SearchBar";
 import "semantic-ui-css/semantic.min.css";
-import api from "../Data/api";
+// import api from "./api";
 import Gallery from "./Gallery";
 import { ImageContext } from "./ImageProvider";
+import axios from "axios";
+require("dotenv").config();
 
 function App() {
   const {setImages} = useContext(ImageContext);
   const onSearchSubmit = async (e) => {
-    const response = await api.get("/search/photos", {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: e },
+      headers: {
+        Authorization: "Client-ID nweoVxPQdGa-w6qfhXvzt2CQh5SH71OzWpReN77Zb7I",
+      },
     });
-    setImages(response.data.results);
+    setImages(res.data.results);
   };
   return (
     <div className="ui container" style={{ marginTop: "10px" }}>
---
+++ src/components/api.js
@@ -0,0 +1,17 @@
+// import axios from "axios"
+
+// export default axios.create({
+//     baseURL: "https://api.unsplash.com",
+//   headers: {
+//     Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`,
+//   },
+// });
+
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://api.unsplash.com",
+  headers: {
+    Authorization: "Client-ID nweoVxPQdGa-w6qfhXvzt2CQh5SH71OzWpReN77Zb7I",
+  },
+});
